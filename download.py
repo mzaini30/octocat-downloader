add6aa1befd7d8586aff12a5a7e6dad5c9ab9115
mzaini30 <muhzaini30@gmail.com>
2019-07-21 10:16:38 +0800

    oke

---
+++ download.py
@@ -0,0 +1,12 @@
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+
+url = 'https://octodex.github.com/'
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+semua = soup.findAll('img.lazy')
+
+print(semua)
